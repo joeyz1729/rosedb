b15de053fc4ffcfefe93b47fd8fb36b3e4bf88f4
YiZou89 <zouyi89@outlook.com>
2023-06-29 13:36:53 +0800

    storage finished

--- storage/db_meta.go
+++ storage/db_meta.go
@@ -1,1 +1,44 @@
 package storage
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+)
+
+// DBMeta save some meta info of db.
+type DBMeta struct {
+	ActiveWriteOff   map[uint16]int64 `json:"active_write_off"`
+	ReclaimableSpace map[uint32]int64 `json:"reclaimable_space"`
+}
+
+// LoadMeta load db meta from file.
+func LoadMeta(path string) (meta *DBMeta) {
+	meta = &DBMeta{
+		ActiveWriteOff:   make(map[uint16]int64),
+		ReclaimableSpace: make(map[uint32]int64),
+	}
+
+	file, err := os.OpenFile(path, os.O_RDONLY, 0600)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	b, _ := ioutil.ReadAll(file)
+	_ = json.Unmarshal(b, meta)
+	return
+
+}
+
+// Store store db meta as json.
+func (meta *DBMeta) Store(path string) error {
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		return err
+	}
+
+	b, _ := json.Marshal(meta)
+	_, err = file.Write(b)
+	return err
+}
