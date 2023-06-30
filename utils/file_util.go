30cfda678935697f6b7bd6e568d3f3d818b22313
YiZou89 <zouyi89@outlook.com>
2023-06-30 10:31:52 +0800

    add utils

---
+++ utils/file_util.go
@@ -0,0 +1,79 @@
+package utils
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"path"
+)
+
+// Exist check dir or file exists.
+func Exist(path string) bool {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+func CopyDir(src, dst string) error {
+	var (
+		err     error
+		dir     []os.FileInfo
+		srcInfo os.FileInfo
+	)
+
+	if srcInfo, err = os.Stat(src); err != nil {
+		return err
+	}
+	if err = os.MkdirAll(dst, srcInfo.Mode()); err != nil {
+		return err
+	}
+	if dir, err = ioutil.ReadDir(src); err != nil {
+		return err
+	}
+
+	for _, fd := range dir {
+		srcPath := path.Join(src, fd.Name())
+		dstPath := path.Join(dst, fd.Name())
+
+		if fd.IsDir() {
+			if err = CopyDir(srcPath, dstPath); err != nil {
+				return err
+			}
+		} else {
+			if err = CopyFile(srcPath, dstPath); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func CopyFile(src, dst string) error {
+	var (
+		err     error
+		srcFile *os.File
+		dstFile *os.File
+		srcInfo os.FileInfo
+	)
+
+	if srcFile, err = os.Open(src); err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	if dstFile, err = os.Create(dst); err != nil {
+		return err
+	}
+	defer dstFile.Close()
+
+	if _, err = io.Copy(dstFile, srcFile); err != nil {
+		return err
+	}
+
+	if srcInfo, err = os.Stat(src); err != nil {
+		return err
+	}
+
+	return os.Chmod(dst, srcInfo.Mode())
+}
